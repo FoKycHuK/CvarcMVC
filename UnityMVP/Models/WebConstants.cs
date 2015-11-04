62b3b6bda6c0af21e6e04fec2ff66c9a9cb474a3
FoKycHuK <fokychuk47@ya.ru>
2015-11-05 04:57:36 +0500

    init

---
+++ UnityMVP/Models/WebConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnityMVP.Models
+{
+    public static class WebConstants
+    {
+        public static string SuperAdminLogin = "admin";
+        public static string SuperAdminPassword = "somePassword";
+    }
+}
