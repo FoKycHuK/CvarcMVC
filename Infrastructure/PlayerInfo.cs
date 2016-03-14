4295c2085d8f2318eab5f8b7cc4eedfd1b3fc8a8
FoKycHuK <fokychuk47@ya.ru>
2016-03-14 13:47:18 +0500

    + Infrastructure

---
+++ Infrastructure/PlayerInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CVARC.Infrastructure
+{
+    [DataContract]
+    [Serializable]
+    public class PlayerInfo
+    {
+        [DataMember]
+        public string CvarcTag { get; set; }
+        [DataMember]
+        public string NickName { get; set; }
+        [DataMember]
+        public bool Connected { get; set; }
+        [DataMember]
+        public int Score { get; set; }
+    }
+}
---
+++ Infrastructure/Serializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace CVARC.Infrastructure
+{
+    //этот класс -- полная копия JSonSerializer из Cvarc.Core
+    //я боюсь переносить его сюда и юзать в коре.
+    //к тому же, это сейчас невозможно из-за циклической зависимости :(
+    public static class Serializer
+    {
+        public static void Serialize(object obj, Stream stream)
+        {
+            new DataContractJsonSerializer(obj.GetType()).WriteObject(stream, obj);
+        }
+
+        public static object Deserialize(Type type, Stream stream)
+        {
+                return new DataContractJsonSerializer(type).ReadObject(stream);
+        }
+    }
+}
